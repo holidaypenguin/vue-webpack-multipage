07e868e1939c48e4d3f54f2c0af12550425ed1d6
holidaypenguin <songship1221@sina.com>
2019-04-09 11:44:52 +0800

    Update：config文件夹下的const位转换

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -48,7 +48,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test'),
+        include: [resolve('src'), resolve('test'), resolve('config'),
           resolve('node_modules/webpack-dev-server/client')],
       },
       {
